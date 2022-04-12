e2f2504e9cee126b85a27510571fec1f8354ee70
yubinwang11 <1102030729@qq.com>
2022-04-12 20:06:28 +0800

    test controller

--- test_controller.py
+++ test_controller.py
@@ -7,7 +7,7 @@ from TrajGen import trajGenerator, Helix_waypoints, Circle_waypoints
 from Quadrotor import QuadSim
 import controller
 np.random.seed(8)
-
+'''
 # 3D boxes   lx, ly, lz, hx, hy, hz
 obstacles = [[-5, 25, 0, 20, 35, 60],
              [30, 25, 0, 55, 35, 100],
@@ -30,10 +30,13 @@ rrt = RRTStar(start = start, goal = goal,
               goal_sample_rate = 0.1)
 
 waypoints, min_cost = rrt.plan()
-
+'''
 
 #scale the waypoints to real dimensions
-waypoints = 0.02*waypoints
+#waypoints = 0.02*waypoints
+start = np.array([0,0,0])
+goal = np.array([1,1,1])
+waypoints = np.stack((start, goal))
 print(waypoints)
 
 #Generate trajectory through waypoints
@@ -52,7 +55,7 @@ ax.set_ylim((0,2))
 ax.set_zlim((0,2))
 
 #plot the waypoints and obstacles
-rrt.draw_path(ax, waypoints)
+# rrt.draw_path(ax, waypoints)
 #mapobs.plotobs(ax, scale = 0.02)
 
 #run simulation
